88c1535f66b3f0b38a46d2f7a84009c49ffacffe
FukeKazki <kazkichi0906@gmail.com>
2019-05-05 23:45:25 +0900

    ABC117 A

---
+++ ABC/A-EntranceExamination.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double T, X;
+    cin >> T >> X;
+    printf("%.4lf\n", T/X);
+    // cout << double(T/X) << endl;
+    return 0;
+}

8a4ead62c12f25f24779c5f31dcbf148cf507e06
FukeKazki <kazkichi0906@gmail.com>
2019-05-12 16:20:43 +0900

    diverta 2019 B

--- ABC/B-RGBBoxes.cpp
+++ ABC/B-RGBBoxes.cpp
@@ -6,22 +6,28 @@ int main() {
     int R, G, B, N;
     cin >> R >> G >> B >> N;
     int cnt = 0;
-    if(R < G) {
-        swap(R, G);
-    }
-    if(G < B) {
-        swap(G, B);
-    }
-    if(R < G) {
-        swap(R, G);
-    }
-    for(int i = 0; i <= N/R; i++) {
-        if(R*i == N) {cnt++; continue;}
-        for(int j = 0; j <= N/G; j++) {
-            if(R*i + G*j == N) {cnt++; continue;}
-            for(int k = 0; k <= N/B; k++) {
-                if(R*i + G*j + B*k == N) {cnt++; break;}
-            }
+    // if(R < G) {
+    //     swap(R, G);
+    // }
+    // if(G < B) {
+    //     swap(G, B);
+    // }
+    // if(R < G) {
+    //     swap(R, G);
+    // }
+    // for(int i = 0; i <= N/R; i++) {
+    //     if(R*i == N) {cnt++; continue;}
+    //     for(int j = 0; j <= N/G; j++) {
+    //         if(R*i + G*j == N) {cnt++; continue;}
+    //         for(int k = 0; k <= N/B; k++) {
+    //             if(R*i + G*j + B*k == N) {cnt++; break;}
+    //         }
+    //     }
+    // }
+    for(int i = 0; i <= N; i++) {
+        for(int j = 0; j <= N; j++) {
+            int tmp = N-R*i-G*j;
+            if(tmp >= 0 && tmp % B == 0) cnt++;
         }
     }
     cout << cnt << endl;

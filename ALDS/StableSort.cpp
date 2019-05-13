4621459d15c4ccafa64e3b141860a5eaa9f04fcf
FukeKazki <kazkichi0906@gmail.com>
2019-05-14 00:02:37 +0900

    ALDS

--- ALDS/StableSort.cpp
+++ ALDS/StableSort.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
-BubbleSort(int C, int N) {
+void BubbleSort(string* array, int N) {
     for(int i = 0; i < N; i++) {
-        for(int j = N-1; j > i; j--) {
-            if(C[j] < C[j-1]) {
-                swap(C[j], C[j-1]);
-            }
-        }
+        for(int j = N-1; j > i; j--) if (array[j][1] < array[j-1][1]) swap(array[j], array[j-1]);
     }
 }
-
-SelectionSort(int C, int N) {
+void SelectionSort(string* array, int N) {
     for(int i = 0; i < N; i++) {
-        minj = i;
-        for(j = i; j < N; j++) {
-            if(C[j] < C[minj]) minj = i;
-        }
-        swap(C[i], C[minj]);
+        int minj = i;
+        for(int j = i; j < N; j++) if(array[j][1] < array[minj][1]) minj = j;
+        swap(array[i], array[minj]);
     }
 }
 int main() {
     int n;
     cin >> n;
-    int A[n];
+    string A[n];
     for(int i = 0; i < n; i++) cin >> A[i];
+    string B[n];
+    memcpy(B, A, sizeof(A));
     BubbleSort(A, n);
-    SelectionSort(A, n);
+    SelectionSort(B, n);
+    for(int i = 0; i < n; i++) i != n-1 ? cout << A[i] << ' ' : cout << A[i] << endl;
+    cout << "Stable" << endl;
+    for(int i = 0; i < n; i++) i != n-1 ? cout << B[i] << ' ' : cout << B[i] << endl;
+    bool flag = false;
+    for(int i = 0; i < n; i++) if(A[i] != B[i]) flag = true;
+    flag ? cout << "Not stable" << endl : cout << "Stable" << endl;
+    
     return 0;
 }

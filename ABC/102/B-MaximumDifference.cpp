fdabe3853bf697f107fdd885067e8a61b24c28bc
FukeKazki <kazkichi0906@gmail.com>
2019-07-14 17:37:29 +0900

    102

---
+++ ABC/102/B-MaximumDifference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    for(int i = 0; i < N; i++) cin >> A[i];
+    sort(A, A+N);
+    cout << A[N-1] - A[0] << endl;
+}

1b8ee5320088c16d1e86b1f1d74f2b4160112b62
FukeKazki <kazkichi0906@gmail.com>
2019-09-01 13:15:25 +0900

    Update: template

--- template.cpp
+++ template.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <list>
-#include <cmath>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
-
 #define REP(i, n) for(int i = 0; i < (n); ++i)
+#define REPR(i, n) for (int i = n; i >= 0; --i)
+#define FOR(i, n, m) for (int i = m; i < n; i++)
+#define INF 1e9
+#define ALL(v) v.begin(), v.end()
 
-int main() {
-
+typedef long long ll;
+/**
+ * C問以降は ll を使う.
+ * double型にするのを忘れない.
+ * nC2の求め方 (1/2)*n*(n-1)
+ */
+int main()
+{
 
     return 0;
 }

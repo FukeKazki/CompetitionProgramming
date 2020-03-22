3635deccc480f0fb0b72250c0bfc04b2b65b7d0d
FukeKazki <kazkichi0906@gmail.com>
2020-03-22 16:34:53 +0900

    Add: ABC152

---
+++ ABC/152/B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define REPR(i, n) for (int i = n; i >= 0; --i)
+#define FOR(i, n, m) for (int i = m; i < n; i++)
+#define MM(i, n, m) for (int i = m; i <= n; i++)
+#define INF 1e9
+#define ALL(v) v.begin(), v.end()
+
+typedef long long ll;
+
+/**
+ * C問以降は ll を使う.
+ * double型にするのを忘れない.
+ * nC2の求め方 (1/2)*n*(n-1)
+ * vector<型> 変数名(要素数, 初期値)
+ * pair<型, 型> 変数名
+ * map<型, 型> 変数名  // 連想配列
+ * priority_queue<型> 変数名  // 最大値から出てくるヒープ
+*/
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    string s1;
+    string s2;
+    REP(i, b) {
+        s1 += char(b);
+    }
+    REP(i, a) {
+        s2 += char(a);
+    }
+    cout << s1 << " " << s2 << endl;
+    return 0;
+}

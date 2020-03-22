9f61f949e327800c79ef8190b882f6d27c9e1b01
FukeKazki <kazkichi0906@gmail.com>
2020-03-22 16:38:07 +0900

    Add: 147

---
+++ ABC/147/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define REPR(i, n) for (int i = n; i >= 0; --i)
+#define FOR(i, n, m) for (int i = m; i < n; i++)
+#define INF 1e9
+#define ALL(v) v.begin(), v.end()
+
+typedef long long ll;
+/**
+ * C問以降は ll を使う.
+ * double型にするのを忘れない.
+ * nC2の求め方 (1/2)*n*(n-1)
+ * vector<型> 変数名(要素数, 初期値)
+*/
+int main()
+{
+    string S;
+    cin >> S;
+    int n = 0;
+    int a = 0;
+    int b = S.length()-1;
+    REP(i, S.length()/2) {
+        if(S.at(a) != S.at(b)) {
+            n++;
+        }
+        a++;
+        b--;
+    }
+    cout << n << endl;
+    return 0;
+}
